288e96f0ec4438d3ed742072aaf3b695a5ae2dcb
ghst659 <ghst659@github.com>
2017-11-11 19:53:25 -0800

    Static vars.

--- src/test/java/tc/pathsub/TextSubstTest.java
+++ src/test/java/tc/pathsub/TextSubstTest.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class TextSubstTest {
-    private final String[] LINES = {
+    private static final String[] LINES = {
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, ",
         "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
         "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris ",
@@ -15,7 +15,7 @@ public class TextSubstTest {
         "dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, ",
         "sunt in culpa qui officia deserunt mollit anim id est laborum."
     };
-    private final String TEXT = String.join("\n", LINES);
+    private static final String TEXT = String.join("\n", LINES);
 
     @Test
     public void testTransform() {
@@ -23,6 +23,7 @@ public class TextSubstTest {
         xfm.put("dolor", "SADNESS");
         xfm.put("id est laborum.", "ad infinitum!");
         xfm.put("Lorem ipsum", "Via dolorosa... ");
+        xfm.put("sunt in culpa qui officia deserunt ", "");
         TextSubst ps = new TextSubst(xfm);
         String found = ps.transform(TEXT);
         System.err.println(found);
